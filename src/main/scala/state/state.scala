937a4212ac1350928d85634ca5cbe7403733ebeb
Duy Pham <duy.pham@zalando.de>
2020-12-01 23:33:31 +0100

    update on chapter 6

--- src/main/scala/state/state.scala
+++ src/main/scala/state/state.scala
@@ -46,4 +46,33 @@ object State {
     val generatedInts = xs.map(_._1)
     (generatedInts, xs.last._2)
   }
+
+  def unit[A](a: A): Rand[A] =
+    rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def nonNegativeEven: Rand[Int] =
+    map(nonNegativeInt)(i => i - i % 2)
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+  rng => {
+    val (a, arng) = ra(rng)
+    val (b, brng) = rb(arng)
+    (f(a, b), brng)
+  }
+
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map2(ra, rb)((_, _))
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight[Rand[List[A]]](unit(List()))((rng: Rand[A], rands: Rand[List[A]]) => map2(rng, rands)(_ :: _))
+
+  def intsUsingSequence(count: Int): Rand[List[Int]] = sequence(List.fill(count)(unit(0)))
+
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => map(f)(g)(rng)
 }
